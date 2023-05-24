1162c250a26c8abd5b94fa76476c7992a915da51
maner92 <emrzrz1502@gmail.com>
2023-05-24 01:40:25 -0600

    Fix build v6

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
+  purge: ["./src/**/*.html", "./src/**/*.js", "./src/**/*.jsx"],
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
